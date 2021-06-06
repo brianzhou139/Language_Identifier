e65a72ba3ded11fb7e598c88b45f6e685bfb5ee7
Brian Zhou <39838697+brianzhou139@users.noreply.github.com>
2021-06-06 02:45:19 +0100

    Add files via upload

--- tgcat.c
+++ tgcat.c
@@ -1,4 +1,4 @@
-//#include "tgcat.h"
+#include "tgcat.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,75 +46,6 @@
 #define TAGBANWA(t) (t>=5984 && t<=6015)
 #define KHMER(t) (t>=6016 && t<=6143)
 #define LIMBU(t) (t>=6400 && t<=6463)
-
-
-enum TgcatCategory {
-  TGCAT_CATEGORY_ART_AND_DESIGN,
-  TGCAT_CATEGORY_BETS_AND_GAMBLING,
-  TGCAT_CATEGORY_BOOKS,
-  TGCAT_CATEGORY_BUSINESS_AND_ENTREPRENEURSHIP,
-  TGCAT_CATEGORY_CARS_AND_OTHER_VEHICLES,
-  TGCAT_CATEGORY_CELEBRITIES_AND_LIFESTYLE,
-  TGCAT_CATEGORY_CRYPTOCURRENCIES,
-  TGCAT_CATEGORY_CULTURE_AND_EVENTS,
-  TGCAT_CATEGORY_CURIOUS_FACTS,
-  TGCAT_CATEGORY_DIRECTORIES_OF_CHANNELS_AND_BOTS,
-  TGCAT_CATEGORY_ECONOMY_AND_FINANCE,
-  TGCAT_CATEGORY_EDUCATION,
-  TGCAT_CATEGORY_EROTIC_CONTENT,
-  TGCAT_CATEGORY_FASHION_AND_BEAUTY,
-  TGCAT_CATEGORY_FITNESS,
-  TGCAT_CATEGORY_FOOD_AND_COOKING,
-  TGCAT_CATEGORY_FOREIGN_LANGUAGE_LEARNING,
-  TGCAT_CATEGORY_HEALTH_AND_MEDICINE,
-  TGCAT_CATEGORY_HISTORY,
-  TGCAT_CATEGORY_HOBBIES_AND_ACTIVITIES,
-  TGCAT_CATEGORY_HOME_AND_ARCHITECTURE,
-  TGCAT_CATEGORY_HUMOR_AND_MEMES,
-  TGCAT_CATEGORY_INVESTMENTS,
-  TGCAT_CATEGORY_JOB_LISTINGS,
-  TGCAT_CATEGORY_KIDS_AND_PARENTING,
-  TGCAT_CATEGORY_MARKETING_AND_PR,
-  TGCAT_CATEGORY_MOTIVATION_AND_SELF_DEVELOPMENT,
-  TGCAT_CATEGORY_MOVIES,
-  TGCAT_CATEGORY_MUSIC,
-  TGCAT_CATEGORY_OFFERS_AND_PROMOTIONS,
-  TGCAT_CATEGORY_PETS,
-  TGCAT_CATEGORY_POLITICS_AND_INCIDENTS,
-  TGCAT_CATEGORY_PSYCHOLOGY_AND_RELATIONSHIPS,
-  TGCAT_CATEGORY_REAL_ESTATE,
-  TGCAT_CATEGORY_RECREATION_AND_ENTERTAINMENT,
-  TGCAT_CATEGORY_RELIGION_AND_SPIRITUALITY,
-  TGCAT_CATEGORY_SCIENCE,
-  TGCAT_CATEGORY_SPORTS,
-  TGCAT_CATEGORY_TECHNOLOGY_AND_INTERNET,
-  TGCAT_CATEGORY_TRAVEL_AND_TOURISM,
-  TGCAT_CATEGORY_VIDEO_GAMES,
-  TGCAT_CATEGORY_OTHER
-};
-
-
-struct TelegramChannelInfo {
-  /**
-   * Title of the channel. A null-terminated string in UTF-8 encoding.
-   */
-  const char *title;
-
-  /**
-   * Description of the channel. A null-terminated string in UTF-8 encoding.
-   */
-  const char *description;
-
-  /**
-   * Number of available channel posts.
-   */
-  size_t post_count;
-
-  /**
-   * List of post_count channel posts. Posts are null-terminated strings in UTF-8 encoding.
-   */
-  const char **posts;
-};
 
 int tgcat_init() {
   return 0;
@@ -235,7 +166,6 @@ int tgcat_detect_language(const struct TelegramChannelInfo *channel_info,
         char *defText="ar";
         strcpy(result_code,receiveLangCode(text,defText,highest_index));
     }else if(highest_index==2){
-        //check russian here
         char *defText="ru";
         strcpy(result_code,receiveLangCode(text,defText,highest_index));
     }else if(highest_index==3){
